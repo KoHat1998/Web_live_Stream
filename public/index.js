5ba926e617ae6046ea09e6197d79f49d15979efd
Ko Hat <htetaungthant1998@gmail.com>
2025-08-07 11:12:16 +0700

    Check TURN server

--- public/index.js
+++ public/index.js
@@ -2,18 +2,18 @@ let localStream = null;
 let peer = null;
 
 const iceServers = [
-  { urls: [ "stun:ss-turn1.xirsys.com" ] },
-  {
-    username: "i4ZrjB737SBvzAuBytor5ByRL1wlwInKspSxUsvccOtE3Xzb4cu_ELZK73SuOeV2AAAAAGiUD_9Lb0hhdA==",
-    credential: "9ec69d0c-7336-11f0-99a0-0242ac140004",
-    urls: [
-      "turn:ss-turn1.xirsys.com:80?transport=udp",
-      "turn:ss-turn1.xirsys.com:3478?transport=udp",
-      "turn:ss-turn1.xirsys.com:80?transport=tcp",
-      "turn:ss-turn1.xirsys.com:3478?transport=tcp",
-      "turns:ss-turn1.xirsys.com:443?transport=tcp",
-      "turns:ss-turn1.xirsys.com:5349?transport=tcp"
-    ]
+  { urls: [ "stun:ss-turn1.xirsys.com" ]
+}, {
+   username: "i4ZrjB737SBvzAuBytor5ByRL1wlwInKspSxUsvccOtE3Xzb4cu_ELZK73SuOeV2AAAAAGiUD_9Lb0hhdA==",
+   credential: "9ec69d0c-7336-11f0-99a0-0242ac140004",
+   urls: [
+       "turn:ss-turn1.xirsys.com:80?transport=udp",
+       "turn:ss-turn1.xirsys.com:3478?transport=udp",
+       "turn:ss-turn1.xirsys.com:80?transport=tcp",
+       "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+       "turns:ss-turn1.xirsys.com:443?transport=tcp",
+       "turns:ss-turn1.xirsys.com:5349?transport=tcp"
+   ]
   }
 ];
 
